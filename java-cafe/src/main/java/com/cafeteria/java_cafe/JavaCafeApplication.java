dfb58ef4d7eb90db293cdcaebef1f53fd7dc023d
Joao Carlos Pontes <jcarlos.pontes@aluno.uece.br>
2025-06-15 23:13:22 -0300

    Adição inicial do projeto backend com configuração em docker.

---
+++ java-cafe/src/main/java/com/cafeteria/java_cafe/JavaCafeApplication.java
@@ -0,0 +1,13 @@
+package com.cafeteria.java_cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCafeApplication.class, args);
+	}
+
+}
